eaaa54b7f6eb0d381e8874278135c1c394a07bc8
Michael Lachut <michael@lachut.com>
2023-10-31 21:56:01 +0000

    Remove Mac builds (I cannot be bothered)

--- forge.config.ts
+++ forge.config.ts
@@ -1,6 +1,5 @@
 import type { ForgeConfig } from '@electron-forge/shared-types';
 import { MakerSquirrel } from '@electron-forge/maker-squirrel';
-import { MakerPKG } from '@electron-forge/maker-pkg';
 import { MakerDeb } from '@electron-forge/maker-deb';
 import { MakerRpm } from '@electron-forge/maker-rpm';
 import { WebpackPlugin } from '@electron-forge/plugin-webpack';
@@ -11,7 +10,7 @@ import { rendererConfig } from './webpack.renderer.config';
 const config: ForgeConfig = {
   packagerConfig: {},
   rebuildConfig: {},
-  makers: [new MakerSquirrel({}), new MakerPKG({}), new MakerRpm({}), new MakerDeb({})],
+  makers: [new MakerSquirrel({}), new MakerRpm({}), new MakerDeb({})],
   plugins: [
     new WebpackPlugin({
       mainConfig,
